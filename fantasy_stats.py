446422cfefeffa8d03b6e7f90c0611e65f616b35
Leonardo <leonardofonsecap@usp.br>
2021-09-21 11:09:52 -0300

    Connection established. Overall stadings done, with parser. Transactions needs updating once real ones are made.

---
+++ fantasy_stats.py
@@ -0,0 +1,186 @@
+from yahoo_oauth import OAuth2
+import json
+from json import dumps, loads
+import datetime
+from pathlib import Path
+
+class Yahoo_Api():
+    def __init__(self, consumer_key, consumer_secret,
+                access_key):
+        self._consumer_key = consumer_key
+        self._consumer_secret = consumer_secret
+        self._access_key = access_key
+        self._authorization = None
+    def _login(self):
+        global oauth
+        oauth = OAuth2(None, None, from_file='./auth/oauth2yahoo.json')
+        if not oauth.token_is_valid():
+            oauth.refresh_access_token()
+
+class ConvertJson():
+
+    def StandingsParse(json):
+        aux = json[1]['standings'][0]['teams'] #[i]['team'] to iterate through all teams
+        teams = []
+        # for i in range(num_teams): for when all teams are in
+        for i in range(8):
+            team = {"Name": aux[str(i)]['team'][0][2]['name'],
+            "Team logo": aux[str(i)]['team'][0][5]['team_logos'][0]['team_logo']['url'],
+            "Nickname": aux[str(i)]['team'][0][19]['managers'][0]['manager']['nickname'],
+            "Rank": aux[str(i)]['team'][2]['team_standings']['rank'],
+            "Wins": int(aux[str(i)]['team'][2]['team_standings']['outcome_totals']['wins']),
+            "Losses": int(aux[str(i)]['team'][2]['team_standings']['outcome_totals']['losses']),
+            "Percentage": aux[str(i)]['team'][2]['team_standings']['outcome_totals']['percentage'],
+            #"Percentage": float(aux[str(i)]['team'][2]['team_standings']['outcome_totals']['percentage']),
+            "Points For": float(aux[str(i)]['team'][2]['team_standings']['points_for']),
+            "Points Against": float(aux[str(i)]['team'][2]['team_standings']['points_against'])}
+            teams.append(team)
+        #print(sorted(teams, key=lambda x: (-x['Percentage'], ['Points For'])))
+
+        data = {
+            "current_week": json[0]['current_week'], #works
+            "standings": sorted(teams, key=lambda x: (-x['Wins'], ['Points For'])) #works, order by percentage when league starts
+        }
+        return data
+
+    def TransactionsParse(json):
+        transactions = []
+        for i in range(json[1]['transactions']['count']):
+            transaction= {
+                "id": json[1]['transactions'][str(i)]['transaction'][0]['transaction_id'],
+                "type": json[1]['transactions'][str(i)]['transaction'][0]['type'],
+                }
+            transactions.append(transaction)
+        data = {
+            "Number Of Transactions": json[1]['transactions']['count'],
+            "Transactions": transactions
+        }
+        return data
+
+
+class UpdateData():
+    def __init__(self):
+        print("init")
+
+    def UpdateLeagueStandings(self):
+        # STANDINGS
+        yahoo_api._login()
+        url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+game_key+'.l.'+league_id+'/standings'
+        response = oauth.session.get(url, params={'format': 'json'})
+        r = response.json()
+        data = ConvertJson.StandingsParse(r['fantasy_content']['league'])
+        week = data['current_week']
+        print('/standings/week'+ week + ".json")
+        with open('./standings/week'+ week + ".json", 'w') as outfile:
+            json.dump(data, outfile)
+        
+        return
+    
+    def UpdateLeagueTransactions(self):
+        yahoo_api._login()
+        url = 'https://fantasysports.yahooapis.com/fantasy/v2/league/'+game_key+'.l.'+league_id+'/transactions'
+        response = oauth.session.get(url, params={'format': 'json'})
+        r = response.json()
+        data = ConvertJson.TransactionsParse(r['fantasy_content']['league'])
+        path = './transactions/Transactions.json'
+        new = data
+        if Path(path).is_file():
+            load_file = open('./transactions/Transactions.json') # load old_transactions
+            old_transactions = json.load(load_file)
+            new = dict(set(data) - set(old_transactions))
+            load_file.close()
+        with open(path, 'w') as outfile:
+            json.dump(data, outfile)
+        with open('./transactions/newTransactions.json', 'w') as newTransactions:
+            json.dump(new, newTransactions)
+        
+        return
+
+    def UpdateYahooLeagueInfo(self):
+        # UPDATE LEAGUE GAME ID
+        yahoo_api._login()
+        url = 'https://fantasysports.yahooapis.com/fantasy/v2/game/nba'
+        response = oauth.session.get(url, params={'format': 'json'})
+        r = response.json()
+        with open('YahooGameInfo.json', 'w') as outfile:
+            json.dump(r, outfile)
+            
+        global game_key
+        game_key = r['fantasy_content']['game'][0]['game_key'] # game key as type-string
+        return
+
+def CurrentWeek():
+    current_week = 1
+    #with open('./league.json', 'r') as fobj:
+    #    info = json.load(fobj)
+    #current_week = info['fantasy_content']['league'][0]['current_week']
+    return current_week
+
+
+
+### WHERE ALL THE MAGIC HAPPENS #########
+
+def main():
+##### Get Yahoo Auth ####
+
+    # Yahoo Keys
+    with open('./auth/oauth2yahoo.json') as json_yahoo_file:
+        auths = json.load(json_yahoo_file)
+    yahoo_consumer_key = auths['consumer_key']
+    yahoo_consumer_secret = auths['consumer_secret']
+    yahoo_access_key = auths['access_token']
+    #yahoo_access_secret = auths['access_token_secret']
+    json_yahoo_file.close()
+
+#### Declare Yahoo, and Current Week Variable ####
+
+
+    global yahoo_api
+    yahoo_api = Yahoo_Api(yahoo_consumer_key, yahoo_consumer_secret, yahoo_access_key)#, yahoo_access_secret)
+
+    global current_week
+    current_week = CurrentWeek()
+
+    with open('./Initial_Setup/league_info_form.txt', 'r') as f:
+        rosters = eval(f.read())
+
+    global num_teams
+    num_teams = rosters['num_teams']
+
+    global num_weeks
+    num_weeks = rosters['num_weeks']
+    
+    global league_id
+    league_id = str(rosters['league_id'])
+
+#### Where the tweets happen ####
+    bot = Bot(yahoo_api)
+    bot.run()
+
+
+class Bot():
+    def __init__(self, yahoo_api):
+        self._yahoo_api = yahoo_api
+
+    def run(self):
+        # Data Updates
+        UD = UpdateData()
+                           
+        UD.UpdateYahooLeagueInfo()
+        print('Yahoo League Info Updated')
+        UD.UpdateLeagueStandings()
+        print('Standings update - Done')
+        UD.UpdateLeagueTransactions()
+        print('Transactions update - Done')
+                                        
+        print('Update Complete')
+
+if __name__ == "__main__":
+    main()
+
+    try:
+        pass
+    except Exception as e:
+        raise
+    else:
+        pass
--- fantasytracker/src/Components/Header/index.jsx
+++ fantasytracker/src/Components/Header/index.jsx
@@ -1,14 +1,15 @@
 function Header(){
-
     return(
         <header>
             <div className = "logo">
             <a Link to = "/">Fantasy Tracker</a>
             </div>
             <ul className = "nav">
-                <li><a Link to ="/">Classificação</a></li>
-                <li><a Link to ="/">Prêmios</a></li>
-                <li><a Link to ="/">Jogadores</a></li>
+                <li><a Link to ="/">Standings</a></li>
+                <li><a Link to ="/">Award Race</a></li>
+                <li><a Link to ="/">Transactions</a></li>
+                <li><a Link to ="/">Previous Seasons</a></li>
+                <li><a href ="https://twitter.com" className="fa fa-twitter"></a></li>
             </ul>
         </header>
     );
