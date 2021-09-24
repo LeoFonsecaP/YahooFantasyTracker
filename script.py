950ba8c363643e9a7c5a0f44eee03571c59dabd9
Leonardo <leonardofonsecap@usp.br>
2021-09-24 13:02:41 -0300

    Script that automates data. Still needs to add Twitter bot stuff and

---
+++ script.py
@@ -0,0 +1,14 @@
+import os
+import time
+
+os.system('git checkout main')
+exec(open("./fantasy_stats.py").read())
+time.sleep(3)
+os.chdir('./fantasytracker')
+os.system('npm run build')
+os.chdir('..')
+os.system('git add ./fantasytracker/src/Components/Data')
+os.system('git add ./fantasytracker/build')
+os.system('git commit -m "Automatic update on data"')
+os.system('git push -u origin main')
+
