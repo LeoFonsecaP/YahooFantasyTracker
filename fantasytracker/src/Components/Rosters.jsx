f53e516b3c470dcbb2083b1aab8c4ad75676a5b3
Leonardo <leonardofonsecap@usp.br>
2021-09-24 18:00:56 -0300

    Fixed a few things

--- fantasytracker/src/Components/Rosters.jsx
+++ fantasytracker/src/Components/Rosters.jsx
@@ -1,8 +1,5 @@
 import rosters from './Data/rosters/Rosters.json'
 console.log(rosters[0])
-//{rosters.map((roster) =>
-    //{roster.Name} 
-//)}
 function Rosters(){
     return(
         <div className="rosters">
@@ -11,15 +8,8 @@ function Rosters(){
                         <img src = {roster['Team logo']}></img>
                         <h3>{roster.Name}</h3>
                         <h3>{roster.Nickname}</h3>
-                        {rosters[0].players.map((player) =>
+                        {roster.players.map((player) =>
                             <div className="pla">
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
-                                <p>Needs work once draft is done</p>
                                 <p>Needs work once draft is done</p>
                             </div>
                         )}
