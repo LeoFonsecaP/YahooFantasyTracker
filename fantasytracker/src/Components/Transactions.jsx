610d2096db995412c31308c0a6f4a0b18b94d72a
Leonardo <leonardofonsecap@usp.br>
2021-10-04 12:57:47 -0300

    Transactions front

--- fantasytracker/src/Components/Transactions.jsx
+++ fantasytracker/src/Components/Transactions.jsx
@@ -1,8 +1,23 @@
 import transactions from './Data/transactions/Transactions.json'
+console.log(transactions.Transactions.length/2)
 function Transactions(){
     return(
-        <div className="transactions">
-            <img src="https://www.mysafetysign.com/img/lg/S/people-working-caution-sign-s-0831.png"></img>
+        <div className="t">
+            <div className="transactions">
+                <ul>
+                {transactions.Transactions.slice(0,transactions.Transactions.length/2).map((transaction) =>
+                    transaction.Players.map((player) =>
+                        <p>{player.Name} ({player.Positions}): {player.Source} {'->'} {player.Destination}</p>)
+                        )}
+                </ul>
+                <ul>
+                {transactions.Transactions.slice(transactions.Transactions.length/2,transactions.Transactions.length).map((transaction) =>
+                    transaction.Players.map((player) =>
+                        <p>{player.Name} ({player.Positions}): {player.Source} {'->'} {player.Destination}</p>)
+                        )}
+                </ul>
+            </div>
+            <p>Last updated: {transactions['Last Updated']}</p>
         </div>
     );
 }
